e70513c221fbc8b8e7a7ca34e9728cb345bbeebe
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2024-09-09 22:33:49 +0800

    Created the class for Kafka Topic

---
+++ src/main/java/com/nus/iss/tasktracker/util/KafkaTopics.java
@@ -0,0 +1,12 @@
+package com.nus.iss.tasktracker.util;
+
+public class KafkaTopics {
+
+    public  static final String TASK_PROCESSING_GROUP ="task-processing-group";
+    public static final String USER_INFO_CREATED = "UserInfoCreated";
+
+    public static final String USER_INFO_DELETED = "UserInfoDeleted";
+    public static final String GROUP_INFO_CREATED = "GroupInfoCreated";
+    public static final String GROUP_INFO_DELETED = "GroupInfoDeleted";
+
+}
