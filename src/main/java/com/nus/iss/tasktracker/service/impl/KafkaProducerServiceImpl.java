de5d39e43b3179f6636c9b02df89fce1a5e2b95e
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2024-09-09 22:32:59 +0800

    Created KafkaProducerService Implementation

---
+++ src/main/java/com/nus/iss/tasktracker/service/impl/KafkaProducerServiceImpl.java
@@ -0,0 +1,31 @@
+package com.nus.iss.tasktracker.service.impl;
+
+import com.nus.iss.tasktracker.service.KafkaProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class KafkaProducerServiceImpl implements KafkaProducerService {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaProducerServiceImpl(KafkaTemplate<String,String> kafkaTemplate){
+        this.kafkaTemplate=kafkaTemplate;
+    }
+
+    @Override
+    public void sendMessage(String topic, String message) {
+        try {
+            kafkaTemplate.send(topic,message);
+            System.out.println("Message sent successfully to topic " + topic);
+        } catch (Exception e ){
+            System.out.println("Failed to send message: " + e.getMessage());
+            log.error(e.getMessage());
+            throw new RuntimeException("Failed to send message to Kafka");
+        }
+
+    }
+}
